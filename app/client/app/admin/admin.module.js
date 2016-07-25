1f3801b73c92948f3b0952ba3d9afa512e3147cd
Marcin Mrotek <kontakt@marcinmrotek.pl>
2016-07-25 21:21:58 +0100

    [INIT]

---
+++ app/client/app/admin/admin.module.js
@@ -0,0 +1,3 @@
+'use strict';
+
+angular.module('dogzApp.admin', ['dogzApp.auth', 'ui.router']);
---
+++ app/client/app/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('dogzApp', ['dogzApp.auth', 'dogzApp.admin', 'dogzApp.constants', 'ngCookies',
+    'ngResource', 'ngSanitize', 'btford.socket-io', 'ui.router', 'ui.bootstrap',
+    'validation.match'
+  ])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
